182528dbc247d461be6ee48db890aa2560d8a4d0
Greg <gjmooney@asu.edu>
2023-06-16 15:16:17 +0200

    Fix logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,6 +21,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Isabel <span className="sm:block hidden">| QuantStack</span>
+          </p>
         </Link>
       </div>
     </nav>
